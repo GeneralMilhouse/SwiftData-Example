e66aece5eaba20a0748a3fa98577dfea03112a46
Johan Brunsten <johan.brunsten@gmail.com>
2023-08-31 19:18:24 +0200

    Initial Commit

---
+++ SwiftData Example/SwiftData_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftData_ExampleApp.swift
+//  SwiftData Example
+//
+//  Created by Johan Brunsten on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftData_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
